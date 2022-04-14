8a828f7511c034c16f7d31cbc62be3787e743caf
Aidre Cabrera <alscgaming@gmail.com>
2022-04-15 01:27:51 +0800

    NewsAPI modification and necessary changes for Updates section

--- src/components/page-cards/main/TheUpdatesNewsDashboard.vue
+++ src/components/page-cards/main/TheUpdatesNewsDashboard.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- Headline -->
     <article class=" p-10 bg-white mx-72 hover:bg-amber-100 duration-700 ease-in-out">
       <div class="grid grid-rows-1 grid-flow-col">
         <div class=" pb-5 col-span-2">
@@ -21,37 +22,69 @@
       </div>
     </article>
 
-    <div
-      class="grid overflow-hidden mx-72 grid-cols-2 grid-rows-2 gap-2 items-center place-content-center justify-center">
-      <div class="box">
-        <a href="#"
-          class="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-          <div class="">
-            <img class="" v-bind:src="MainArticleJSON.media" alt="">
+    <!-- List Articles -->
+
+    <div>
+      <article v-for="(listArticleInfo, index) in this.ListArticleJSON"
+        class=" p-10 bg-white mx-72 hover:bg-amber-100 duration-700 ease-in-out">
+        <div class="grid grid-rows-1 grid-flow-col">
+          <div class=" pb-5 col-span-2">
+            <h5 @click="navTo(listArticleInfo.link)" class="leading-tight cursor-pointer text-5xl font-bold text-red">
+              {{
+                listArticleInfo.title
+              }}</h5>
           </div>
-          <div class="flex flex-col justify-between p-4 leading-normal">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology
-              acquisitions 2021</h5>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology
-              acquisitions of 2021 so far, in reverse chronological order.</p>
+          <div class="item row-span-1 col-span-2">
+            <p class=" text-ellipsis text-justify font-medium text-gray-700 ">{{
+              listArticleInfo.summary
+            }}</p>
+            <h1 class=" pt-5 font-bold text-black"> {{ listArticleInfo.published_date }} ago</h1>
           </div>
-        </a>
+
+          <div class=" align-top ml-10 row-span-2 col-span-1 w-144">
+            <img class="object-cover" v-bind:src="listArticleInfo.media" :key="index" alt="">
+          </div>
+        </div>
+      </article>
+    </div>
+
+    <!-- <div v-for="(info, index) in this.ListArticleJSON" class="box"><a href="#"
+        class="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+        <img class=" ml-5 object-cover w-72 h-full rounded-xl" :src="info.media" alt="">
+        <div class="flex flex-col justify-between p-4 leading-normal">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> {{ info.title }} </h5>
+          <p class=" mb-3 font-bold text-gray-700 dark:text-gray-400"> {{ ListArticleJSON.published_date }} </p>
+          <p class=" mb-3 font-normal text-gray-700 dark:text-gray-400"> </p>
+        </div>
+      </a>
+    </div> -->
+
+    <!-- <div class=" mx-72">
+      <div class="flex flex-wrap -mx-3 overflow-hidden sm:-mx-2 md:-mx-2 lg:-mx-2 xl:-mx-2">
+
       </div>
-      <div v-for="(info, index) in this.ListArticleJSON" class="box"><a href="#"
-          class="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-          <img class=" ml-5 object-cover w-72 h-full rounded-xl" :src="info.media" alt="">
-          <div class="flex flex-col justify-between p-4 leading-normal">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> {{ info.title }} </h5>
-            <p class=" mb-3 font-bold text-gray-700 dark:text-gray-400"> {{ ListArticleJSON.published_date }} </p>
-            <p class=" mb-3 font-normal text-gray-700 dark:text-gray-400"> </p>
+    </div>
+
+    <div class="grid overflow-hidden grid-cols-2 auto-rows-auto gap-2 w-full">
+      <div class="box">
+        <div
+          class="my-3 px-3 w-1/2 overflow-hidden sm:my-2 sm:px-2 sm:w-1/2 md:my-2 md:px-2 md:w-1/2 lg:my-2 lg:px-2 lg:w-1/2 xl:my-2 xl:px-2 xl:w-1/2">
+          <div v-for="(info, index) in this.ListArticleJSON" class="box"><a href="#"
+              class="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+              <img class=" ml-5 object-cover w-72 h-full rounded-xl" :src="info.media" alt="">
+              <div class="flex flex-col justify-between p-4 leading-normal">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> {{ info.title }}
+                </h5>
+                <p class=" mb-3 font-bold text-gray-700 dark:text-gray-400"> {{ ListArticleJSON.published_date }} </p>
+                <p class=" mb-3 font-normal text-gray-700 dark:text-gray-400"> </p>
+              </div>
+            </a>
           </div>
-        </a></div>
-    </div>
+        </div>
+      </div>
+    </div> -->
 
-    <img v-for="image in images" :src="image.url" :alt="image.alt" />
-    <div class=" grid grid-col-2">
 
-    </div>
   </div>
 </template>
 
@@ -72,7 +105,7 @@ export default {
       method: 'GET',
       url: 'https://api.newscatcherapi.com/v2/search',
       params: {
-        q: '((COVID-19) || (Vaccination))',
+        q: '(((COVID-19) || (Vaccination)) -BBM -UniTeam -Jinggoy -Marcos)',
         lang: 'en',
         sort_by: 'relevancy',
         countries: 'PH',
@@ -83,8 +116,8 @@ export default {
         'x-api-key': 'idjeGtEPBWHUTVvKQp4KBpysFnXdJTiKO8PmKwQdJbM'
       }
     }).then(response => {
-      this.MainArticleJSON = response.data.articles[0];
-      let dateParsing = response.data.articles[0].published_date;
+      this.MainArticleJSON = response.data.articles[13];
+      let dateParsing = response.data.articles[13].published_date;
       dayjs.extend(relativeTime);
       let dateParsed = dayjs(dateParsing).fromNow('h');
       this.MainArticleJSON.published_date = dateParsed;
