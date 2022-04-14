20f9056dc3e640fd8a374b3ec1cf0d22e07435e1
Aidre Cabrera <alscgaming@gmail.com>
2022-04-14 21:20:22 +0800

    List articles for News Updates

--- src/components/page-cards/main/TheUpdatesNewsDashboard.vue
+++ src/components/page-cards/main/TheUpdatesNewsDashboard.vue
@@ -1,10 +1,9 @@
 <template>
   <div>
-    <article class=" p-10 bg-white mx-72 my-0 hover:bg-amber-100 duration-700 ease-in-out">
+    <article class=" p-10 bg-white mx-72 hover:bg-amber-100 duration-700 ease-in-out">
       <div class="grid grid-rows-1 grid-flow-col">
-        <div class=" col-span-2">
-          <h5 @click="navTo(MainArticleJSON.link)"
-            class=" pb-5 leading-tight cursor-pointer text-5xl font-bold text-red">
+        <div class=" pb-5 col-span-2">
+          <h5 @click="navTo(MainArticleJSON.link)" class="leading-tight cursor-pointer text-5xl font-bold text-red">
             {{
               MainArticleJSON.title
             }}</h5>
@@ -16,91 +15,42 @@
           <h1 class=" pt-5 font-bold text-black"> {{ MainArticleJSON.published_date }} ago</h1>
         </div>
 
-        <div class=" align-top ml-10 row-span-2 col-span-1 w-144 h-72">
-          <img class="object-cover " v-bind:src="MainArticleJSON.media" alt="">
+        <div class=" align-top ml-10 row-span-2 col-span-1 w-144">
+          <img class="object-cover" v-bind:src="MainArticleJSON.media" alt="">
         </div>
       </div>
     </article>
-    <div class="">
 
-      <div
-        class="object-cover rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg bg-pink-700 flex flex-wrap justify-center center overflow-hidden sm:-mx-1 md:-mx-1 lg:-mx-1 xl:-mx-1">
-
-
-
-        <article class="px-72">
-          <div>
-            <div class=" grid overflow-hidden grid-cols-5 grid-rows-2 grid-flow-col gap-2">
-              <div id="imgContainer" class="row-span-1 col-span-3 my-72 box bg-pink-700">
-                <h1 @click="navTo(MainArticleJSON.link)" class=" font-extrabold text-5xl text-left p-5 cursor-pointer">
-                  {{ MainArticleJSON.title }}
-                </h1>
-                <div>
-                  <div id="imgContainer" class=" col-span-3 row-span-1 box bg-pink-700">
-                    <div class="p-2 grid overflow-hidden grid-cols-1 grid-rows-2">
-                      <div class="p-5 row-span-1 bg-slate-300 text-black text-justify text-ellipsis">
-                        <span>{{ MainArticleJSON.summary }}</span>
-                      </div>
-                      <div class="p-5 row-span-2 bg-slate-700 box">
-                        <span>{{ MainArticleJSON.author }}</span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <header id="info" class=" h-1/2 flex-shrink col-span-3 row-span-2 object-cover bg-slate-500">
-                <img class=" p-10" v-bind:src="MainArticleJSON.media">
-              </header>
-            </div>
+    <div
+      class="grid overflow-hidden mx-72 grid-cols-2 grid-rows-2 gap-2 items-center place-content-center justify-center">
+      <div class="box">
+        <a href="#"
+          class="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+          <div class="">
+            <img class="" v-bind:src="MainArticleJSON.media" alt="">
           </div>
-        </article>
-      </div>
-
-      <div
-        class="my-1 px-1 w-1/3 overflow-hidden sm:my-1 sm:px-1 sm:w-1/3 md:my-1 md:px-1 md:w-1/2 lg:my-1 lg:px-1 lg:w-1/2 xl:my-1 xl:px-1 xl:w-1/2">
-        <div
-          class="my-1 px-1 w-1/3 overflow-hidden sm:my-1 sm:px-1 sm:w-1/3 md:my-1 md:px-1 md:w-1/2 lg:my-1 lg:px-1 lg:w-1/2 xl:my-1 xl:px-1 xl:w-1/2">
-          <div v-for="ListItem in this.ListArticleJSON">
-            <div>
-              <div class=" grid overflow-hidden grid-cols-2 grid-rows-3 grid-flow-col gap-2">
-                <div id="info" class="row-span-3 box bg-slate-500">
-                  <img v-bind:src="ListItem.media" alt="">
-                </div>
-                <div id="imgContainer" class="row-span-1 box bg-pink-700">
-                  <h1 class="p-5">
-                    {{ ListItem.title }}
-                  </h1>
-                </div>
-                <div id="imgContainer" class="row-span-2 box bg-pink-700">
-                  <div class="p-2 grid overflow-hidden grid-cols-1 grid-rows-2">
-                    <div class="p-5 row-span-1 bg-slate-300 box">
-                      <span>{{ ListItem.summary }}</span>
-                    </div>
-                    <div class="p-5 row-span-2 bg-slate-700 box">
-                      <span>{{ ListItem.author }}</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div class="flex flex-col justify-between p-4 leading-normal">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology
+              acquisitions 2021</h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology
+              acquisitions of 2021 so far, in reverse chronological order.</p>
           </div>
-        </div>
-
-        <div
-          class="my-1 px-1 w-1/3 overflow-hidden sm:my-1 sm:px-1 sm:w-1/3 md:my-1 md:px-1 md:w-1/2 lg:my-1 lg:px-1 lg:w-1/2 xl:my-1 xl:px-1 xl:w-1/2">
-
-        </div>
-
-        <div
-          class="my-1 px-1 w-1/3 overflow-hidden sm:my-1 sm:px-1 sm:w-1/3 md:my-1 md:px-1 md:w-1/2 lg:my-1 lg:px-1 lg:w-1/2 xl:my-1 xl:px-1 xl:w-1/2">
-
-        </div>
-
-        <div
-          class="my-1 px-1 w-1/3 overflow-hidden sm:my-1 sm:px-1 sm:w-1/3 md:my-1 md:px-1 md:w-1/2 lg:my-1 lg:px-1 lg:w-1/2 xl:my-1 xl:px-1 xl:w-1/2">
-
-        </div>
+        </a>
       </div>
+      <div v-for="(info, index) in this.ListArticleJSON" class="box"><a href="#"
+          class="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+          <img class=" ml-5 object-cover w-72 h-full rounded-xl" :src="info.media" alt="">
+          <div class="flex flex-col justify-between p-4 leading-normal">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> {{ info.title }} </h5>
+            <p class=" mb-3 font-bold text-gray-700 dark:text-gray-400"> {{ ListArticleJSON.published_date }} </p>
+            <p class=" mb-3 font-normal text-gray-700 dark:text-gray-400"> </p>
+          </div>
+        </a></div>
+    </div>
+
+    <img v-for="image in images" :src="image.url" :alt="image.alt" />
+    <div class=" grid grid-col-2">
+
     </div>
   </div>
 </template>
@@ -122,7 +72,7 @@ export default {
       method: 'GET',
       url: 'https://api.newscatcherapi.com/v2/search',
       params: {
-        q: 'COVID-19',
+        q: '((COVID-19) || (Vaccination))',
         lang: 'en',
         sort_by: 'relevancy',
         countries: 'PH',
@@ -133,13 +83,17 @@ export default {
         'x-api-key': 'idjeGtEPBWHUTVvKQp4KBpysFnXdJTiKO8PmKwQdJbM'
       }
     }).then(response => {
-      this.MainArticleJSON = response.data.articles[17];
-      let dateParsing = response.data.articles[23].published_date;
+      this.MainArticleJSON = response.data.articles[0];
+      let dateParsing = response.data.articles[0].published_date;
       dayjs.extend(relativeTime);
       let dateParsed = dayjs(dateParsing).fromNow('h');
       this.MainArticleJSON.published_date = dateParsed;
       this.ListArticleJSON = response.data.articles;
-      this.ListArticleJSON.splice(0, 3);
+      let dateParsingList = response.data.articles.published_date;
+      dayjs.extend(relativeTime);
+      let dateParsedList = dayjs(dateParsing).fromNow('h');
+      this.ListArticleJSON.published_date = dateParsedList;
+      this.ListArticleJSON.splice(0, 1);
       // console.log(this.MainArticleJSON);
       // console.log(this.ListArticleJSON);
     })
